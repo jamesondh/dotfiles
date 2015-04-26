3970c24ea2f041bda2ede85d4fcf726d2c91e66f
Jameson Hodge <jamesondh@tuta.io>
2015-04-26 09:34:58 -0400

    Add .emacs.d folder

---
+++ emacs.d/init.el
@@ -0,0 +1,10 @@
+;; Require Emacs' package functionality
+(require 'package)
+
+;; Add the Melpa repository to the list of package sources
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+
+;; Initialise the package system.
+(package-initialize)
+
+(require 'graphene)
