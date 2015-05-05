a0f48793524881cc0ede73391f71488219dd3b9a
Jameson Hodge <jameson@hodge.org>
2015-05-05 12:22:50 -0400

    Create SL3 preferences folder if it doesn't exist

--- .sublime-text-3/setup.sh
+++ .sublime-text-3/setup.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+
+# check if ST3 preferences folder exists
+if [ ! -d ~/.config]; then
+    mkdir ~/.config
+fi
+if [ ! -d ~/.config/sublime-text-3]; then
+    mkdir ~/.config/sublime-text-3
+fi
+if [ ! -d ~/.config/sublime-text-3/Packages]; then
+    mkdir ~/.config/sublime-text-3/Packages
+fi
+if [ ! -d ~/.config/User]; then
+    mkdir ~/.config/sublime-text-3/Packages/User
+fi
+
+# link files
 ln -fvs ~/.dotfiles/.sublime-text-3/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/
 ln -fvs ~/.dotfiles/sublime-text-3/Default\ \(Linux\).sublime-keymap ~/.config/sublime-text-3/Packages/User/
