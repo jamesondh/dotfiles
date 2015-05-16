a781147a68597794af2f7fc40670a4d6f74e895c
Jameson Hodge <jameson@hodge.org>
2015-05-16 13:20:49 -0400

    Create install-bundles.sh script for vim

---
+++ .vim/install-bundles.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/
+git clone https://github.com/scrooloose/nerdtree ~/.vim/bundle/
+git clone https://github.com/bling/vim-airline ~/.vim/bundle/
+git clone https://github.com/bling/vim-bufferline ~/.vim/bundle/
