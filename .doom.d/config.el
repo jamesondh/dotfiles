9894457cd8a6f85185d8a9b60c472a5a183d5fce
Jameson Hodge <contact@jamesonhodge.com>
2021-03-06 15:45:37 -0500

    reset repo; add .doom.d :)

---
+++ .doom.d/config.el
@@ -0,0 +1,99 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Jameson Hodge"
+      user-mail-address "contact@jamesonhodge.com")
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; CUSTOM variables
+(setq
+ doom-font (font-spec :family "Iosevka" :size 14)
+ doom-variable-pitch-font (font-spec :family "Iosevka Aile" :size 14)
+ doom-big-font (font-spec :family "Iosevka Aile" :size 14)
+ projectile-project-search-path '("~/code/")
+ js-indent-level 2
+ typescript-indent-level 2
+)
+
+;; Custom - enable terminal foreground and backgroud colors
+(after! solaire-mode
+  (solaire-global-mode -1))
+(custom-set-faces
+ '(default ((t (:background "#000000", :foreground "#ffffff"))))
+ '(hl-line ((t (:background "#000000", :foreground "#ffffff"))))
+)
+
+;; CUSTOM - duplicate lines
+(defun duplicate-line (arg)
+  "Duplicate current line, leaving point in lower line."
+  (interactive "*p")
+
+  ;; save the point for undo
+  (setq buffer-undo-list (cons (point) buffer-undo-list))
+
+  ;; local variables for start and end of line
+  (let ((bol (save-excursion (beginning-of-line) (point)))
+        eol)
+    (save-excursion
+
+      ;; don't use forward-line for this, because you would have
+      ;; to check whether you are at the end of the buffer
+      (end-of-line)
+      (setq eol (point))
+
+      ;; store the line and disable the recording of undo information
+      (let ((line (buffer-substring bol eol))
+            (buffer-undo-list t)
+            (count arg))
+        ;; insert the line arg times
+        (while (> count 0)
+          (newline)         ;; because there is no newline in 'line'
+          (insert line)
+          (setq count (1- count)))
+        )
+
+      ;; create the undo information
+      (setq buffer-undo-list (cons (cons eol (point)) buffer-undo-list)))
+    ) ; end-of-let
+
+  ;; put the point in the lowest line and return
+  (next-line arg))
+(global-set-key (kbd "C-d") 'duplicate-line)
+
+;; CUSTOM - vterm
+(global-set-key (kbd "C-c 1") 'vterm)
+(global-set-key (kbd "C-c 2") 'treemacs)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/nextcloud/org/")
+(setq org-roam-directory "~/nextcloud/roam/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
--- .vim/install-bundles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -d ~/.dotfiles/.vim/bundle/auto-pairs ]; then
-    git clone https://github.com/jiangmiao/auto-pairs ~/.dotfiles/.vim/bundle/auto-pairs
-fi
-
-if [ ! -d ~/.dotfiles/.vim/bundle/nerdtree ]; then
-    git clone https://github.com/scrooloose/nerdtree ~/.dotfiles/.vim/bundle/nerdtree
-fi
