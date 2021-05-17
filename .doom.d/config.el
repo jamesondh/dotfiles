9b9befca9ff11fb9768e0b098b58387cd49d6f32
Jameson Hodge <contact@jamesonhodge.com>
2021-05-17 10:59:45 -0400

    emacs: cleanup config.el

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,54 +1,39 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-(setq user-full-name "Jameson Hodge"
-      user-mail-address "contact@jamesonhodge.com")
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
-
-;; CUSTOM variables
 (setq
+ user-full-name "Jameson Hodge"
+ user-mail-address "contact@jamesonhodge.com"
+ doom-theme 'doom-one
  doom-font (font-spec :family "Iosevka" :size 14)
  doom-variable-pitch-font (font-spec :family "Iosevka Aile" :size 14)
  doom-big-font (font-spec :family "Iosevka Aile" :size 14)
  projectile-project-search-path '("~/Documents/")
- js-indent-level 2
- typescript-indent-level 2
-)
+ display-line-numbers-type t
+ org-directory "~/.org-server/org/"
+ org-roam-directory "~/.org-server/roam/")
 
-;; Custom - enable terminal foreground and backgroud colors
+;; enable terminal foreground and backgroud colors
 (after! solaire-mode
   (solaire-global-mode -1))
 (custom-set-faces
  '(default ((t (:background "#000000", :foreground "#ffffff"))))
- '(hl-line ((t (:background "#000000", :foreground "#ffffff"))))
-)
+ '(hl-line ((t (:background "#000000", :foreground "#ffffff")))))
 
-;; CUSTOM - duplicate lines
+;; duplicate lines
+;; https://stackoverflow.com/a/998472
 (defun duplicate-line (arg)
   "Duplicate current line, leaving point in lower line."
   (interactive "*p")
-
   ;; save the point for undo
   (setq buffer-undo-list (cons (point) buffer-undo-list))
-
   ;; local variables for start and end of line
   (let ((bol (save-excursion (beginning-of-line) (point)))
         eol)
     (save-excursion
-
       ;; don't use forward-line for this, because you would have
       ;; to check whether you are at the end of the buffer
       (end-of-line)
       (setq eol (point))
-
       ;; store the line and disable the recording of undo information
       (let ((line (buffer-substring bol eol))
             (buffer-undo-list t)
@@ -59,35 +44,18 @@
           (insert line)
           (setq count (1- count)))
         )
-
       ;; create the undo information
       (setq buffer-undo-list (cons (cons eol (point)) buffer-undo-list)))
     ) ; end-of-let
-
   ;; put the point in the lowest line and return
   (next-line arg))
-(global-set-key (kbd "C-c d") 'duplicate-line)
 
-;; CUSTOM - misc keybindings
-(global-set-key (kbd "C-c 1") 'vterm)
-(global-set-key (kbd "C-c 2") 'treemacs)
-
-;; CUSTOM - avy
-(global-set-key (kbd "C-:") 'avy-goto-char)
+;; avy
 (setq avy-timeout-seconds 0.7)
-(global-set-key (kbd "C-'") 'avy-goto-char-timer)
-(global-set-key (kbd "M-g f") 'avy-goto-line)
-(global-set-key (kbd "M-g w") 'avy-goto-word-1)
-(global-set-key (kbd "M-g e") 'avy-goto-word-0)
 (avy-setup-default)
-(global-set-key (kbd "C-c C-j") 'avy-resume)
 
-;; CUSTOM - turn off auto indent for web mode
+;; turn off auto indent for web mode
 (setq web-mode-enable-auto-indentation nil)
-
-;; scroll lines
-(global-set-key (kbd "M-p") 'scroll-down-line)
-(global-set-key (kbd "M-n") 'scroll-up-line)
 
 ;; keep isearch result closer to center of window
 ;; https://emacs.stackexchange.com/a/10432
@@ -107,28 +75,15 @@
       (let ((recenter-position 0.3))
         (recenter '(4)))))
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/.org-server/org/")
-(setq org-roam-directory "~/.org-server/roam/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+;; keybindings
+(global-set-key (kbd "C-c d") 'duplicate-line)
+(global-set-key (kbd "C-c 1") 'vterm)
+(global-set-key (kbd "C-c 2") 'treemacs)
+(global-set-key (kbd "C-:") 'avy-goto-char)
+(global-set-key (kbd "C-'") 'avy-goto-char-timer)
+(global-set-key (kbd "M-g f") 'avy-goto-line)
+(global-set-key (kbd "M-g w") 'avy-goto-word-1)
+(global-set-key (kbd "M-g e") 'avy-goto-word-0)
+(global-set-key (kbd "C-c C-j") 'avy-resume)
+(global-set-key (kbd "M-p") 'scroll-down-line)
+(global-set-key (kbd "M-n") 'scroll-up-line)
